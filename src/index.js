640e38106bb077bb82ad73a8653510ce0c8015a2
timon <timon0596@yandex.ru>
2019-10-05 03:52:40 +0300

    no footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./index.pug"
+import "./components/components.sass"
+import logo from "./img/logo.svg"
+import formbgr from "./img/form_bgr_img.png"
+import gbdd from "./img/gbdd.png"
+import legal from "./img/legal.png"
+import taxes from "./img/taxes.png"
+import services from "./img/services.png"
+let partners = require.context("./img/partners", true, /\.svg$/)
+console.log(partners)
